0542f6d00105b7e29b6e9d1ce61b3aa336022775
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-04-27 11:27:27 +0900

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,23 +1,43 @@
 chrome.runtime.onMessage.addListener(srcUrl =>
   chrome.bookmarks.getChildren("2", async treeNode => {
+    t.placeholder = t.value = srcUrl;
+    t.select();
+
     let rootId = (treeNode.findLast(v => v.title == "photobook64") || await chrome.bookmarks.create({ title: "photobook64" })).id;
     let folders = (await chrome.bookmarks.getChildren(rootId)).filter(v => "url" in v == 0);
     let ids = [];
+
     f.innerHTML =
       folders.length
-        ? folders.reduce((a, v) => (ids.push(a.id), a + "<option>📁 " + v.title), "")
+        ? folders.reduce((a, v) => (ids.push(v.id), a + "<option>📁 " + v.title), "")
         : (chrome.bookmarks.create({ parentId: rootId, title: "images" }), "<option>📁 images");
+    
+    let dataUrl =
+      srcUrl[0] != "d"
+        ? (await new Promise(resolve =>
+            fetch (srcUrl)
+              .then(r => r.blob())
+                .then(r => {
+                  let reader = new FileReader;
+                  reader.onload = resolve;
+                  reader.readAsDataURL(r);
+                })
+          )).target.result
+        : srcUrl;
+
+    let parentId = ids[f.selectedIndex];
+    let oldItem;
+
+    (onselect = () =>
+      chrome.bookmarks.getChildren(parentId = ids[f.selectedIndex], treeNode => (
+        b.textContent = (oldItem = treeNode.find(v => v.url == dataUrl)) ? "Remove" : "Done",
+        b.setAttribute("style", oldItem  ? "filter:hue-rotate(130deg)" : "")
+      ))
+    )();
+
     b.onclick = () =>
-      fetch (srcUrl)
-        .then(r => r.blob())
-          .then(r => {
-            let reader = new FileReader;
-            reader.onload = e => chrome.bookmarks.create({
-              parentId: ids[f.selectedIndex],
-              title: t.value || srcUrl,
-              url: e.target.result
-            })
-            reader.readAsDataURL(r);
-          })
+      oldItem
+        ? chrome.bookmarks.remove({ id: oldItem.id }, close)
+        : chrome.bookmarks.create({ parentId, title: t.value || srcUrl, url: dataUrl }, close);
   })
 );
