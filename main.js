37fde0ee52b8fab6a5cbad62fb348444d210480d
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-05-03 15:20:35 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -4,18 +4,9 @@ chrome.bookmarks.getChildren("2", otherBookmarks => {
     let i = 0;
     while (i < nodes.length) {
       let { url, title } = nodes[i];
-      if (url && url[0] == "d") {
-        let img = new Image;
-        img.src = url;
-        if (URL.canParse(title))
-          document.body.append(img, title)
-        else {
-          let p = document.createElement("p");
-          let index = title.lastIndexOf(" | ");
-          p.textContent =  title.slice(0, index);
-          document.body.append(p, img, title.slice(index + 3));
-        }
-      }
+      let img = new Image;
+      img.src = url;
+      document.body.appendChild(img).title = title;
       ++i;
     }
   });
